cb6d84c15a64e18074b2b5454a6840cdb947ae93
Roiney <nei_beal@hotmail.com>
2022-10-24 18:29:45 -0300

    caramba -1

--- app/backend/src/middlewares/authentication.ts
+++ app/backend/src/middlewares/authentication.ts
@@ -1,3 +1,4 @@
+import { compare } from 'bcryptjs';
 import { Request, Response } from 'express';
 import UserService from '../service/UserService';
 
@@ -10,13 +11,17 @@ async function authentication(req: Request, res: Response) {
     return res.status(400).json({ message: 'All fields must be filled' });
   }
 
-  console.log('teste');
-
-  res.status(200).json({ message: 'ate aqui' });
-
   const login = await service.findEmail(email);
 
   if (!login) return res.status(401).json({ message: 'Incorrect email or password' });
+
+  const isPasswordValid = await compare(password, login.password);
+
+  if (!isPasswordValid) {
+    return res.status(401).json({ message: 'Incorrect email or password' });
+  }
+
+  res.status(200).json({ message: 'ate aqui' });
 }
 
 export default authentication;
