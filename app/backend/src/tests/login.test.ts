979805a4100ea0527b3a647f4039957d6aca2f28
Roiney <nei_beal@hotmail.com>
2022-10-25 16:27:52 -0300

    testando

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -3,6 +3,7 @@ import * as sinon from 'sinon';
 import * as jwt from 'jsonwebtoken';
 import * as bcrypt from 'bcryptjs';
 import User from '../database/models/UserModel';
+import UserService from '../service/UserService';
 
 import chaiHttp = require('chai-http');
 
@@ -17,44 +18,69 @@ import { noPasswordLoginMock, noEmailLoginMock, InvalidEmail, InvalidPassword, m
 describe('/login', () => {
     beforeEach(sinon.restore)
     describe('testando o ENDPOIT /login', () => {
-    it('retorno da menssagem "All fields must be filled" faltando password', async () => {
-        const response = await chai.request(app).post('/login').send(noPasswordLoginMock);
+        it('retorno da menssagem "All fields must be filled" faltando password', async () => {
+            const response = await chai.request(app).post('/login').send(noPasswordLoginMock);
 
-        expect(response.status).to.be.equal(400);     
-        expect(response.body).to.be.deep.equal({ message: 'All fields must be filled' });
-        
+            expect(response.status).to.be.equal(400);
+            expect(response.body).to.be.deep.equal({ message: 'All fields must be filled' });
+
+        })
+        it('retorno da menssagem "All fields must be filled" faltando email', async () => {
+            const response = await chai.request(app).post('/login').send(noEmailLoginMock);
+
+            expect(response.status).to.be.equal(400);
+            expect(response.body).to.be.deep.equal({ message: 'All fields must be filled' });
+
+        })
+        it('retorno da menssagem "Incorrect email or password" email errado', async () => {
+            sinon.stub(User, 'findOne').resolves(null);
+            const response = await chai.request(app).post('/login').send(InvalidEmail);
+
+            expect(response.status).to.be.equal(401);
+            expect(response.body).to.be.deep.equal({ message: 'Incorrect email or password' });
+        })
+        it('retorno da menssagem "Incorrect email or password" senha errado', async () => {
+            sinon.stub(User, 'findOne').resolves(null);
+            const response = await chai.request(app).post('/login').send(InvalidPassword);
+
+            expect(response.status).to.be.equal(401);
+            expect(response.body).to.be.deep.equal({ message: 'Incorrect email or password' });
+        })
+        it('retorno token valido', async () => {
+            sinon.stub(User, 'findOne').resolves(mockFind as User);
+            sinon.stub(jwt, 'sign').resolves(mockToken);
+            sinon.stub(bcrypt, 'compare').resolves(true);
+
+            const response = await chai.request(app).post('/login').send(mockValid);
+
+            expect(response.status).to.be.equal(200);
+            expect(response.body).to.haveOwnProperty('token');
+
+        })
     })
-    it('retorno da menssagem "All fields must be filled" faltando email', async () => {
-        const response = await chai.request(app).post('/login').send(noEmailLoginMock);
 
-        expect(response.status).to.be.equal(400);     
-        expect(response.body).to.be.deep.equal({ message: 'All fields must be filled' });
-        
+    describe('/login/validate', () => {
+        it('Mensagem de erro quando não tiver autorização', async () => {
+            const response = await chai.request(app).get('/login/validate');
+
+            expect(response.status).to.be.equal(401);
+            expect(response.body).to.be.deep.equal({ message: 'unauthorized' })
+        })
+        it('retornando o role', async () => {
+            const userService = new UserService();
+
+            sinon.stub(User, 'findOne').resolves(mockFind as User);
+            sinon.stub(bcrypt, 'compare').resolves(true);
+            sinon.stub(userService, 'getRole').resolves({ role: 'admin' });
+
+            const getToken = await chai.request(app).post('/login').send(mockValid);
+
+            const token = getToken.body.token;
+
+            const response = await chai.request(app).get('/login/validate').set('authorization', token);
+
+            expect(response.status).to.be.equal(200);
+
+        })
     })
-    it('retorno da menssagem "Incorrect email or password" email errado', async () => {
-        sinon.stub(User, 'findOne').resolves(null);
-        const response = await chai.request(app).post('/login').send(InvalidEmail);
-
-        expect(response.status).to.be.equal(401);         
-        expect(response.body).to.be.deep.equal({ message: 'Incorrect email or password' });
-    })
-    it('retorno da menssagem "Incorrect email or password" senha errado', async () => {
-        sinon.stub(User, 'findOne').resolves(null);
-        const response = await chai.request(app).post('/login').send(InvalidPassword);
-
-        expect(response.status).to.be.equal(401);         
-        expect(response.body).to.be.deep.equal({ message: 'Incorrect email or password' });
-    })
-    it('retorno token valido', async () => {
-        sinon.stub(User, 'findOne').resolves(mockFind as User);
-        sinon.stub(jwt, 'sign').resolves(mockToken);
-        sinon.stub(bcrypt, 'compare').resolves(true);
-
-        const response = await chai.request(app).post('/login').send(mockValid);
-
-        expect(response.status).to.be.equal(200); 
-        expect(response.body).to.haveOwnProperty('token');
-
-    })
-})
 })
